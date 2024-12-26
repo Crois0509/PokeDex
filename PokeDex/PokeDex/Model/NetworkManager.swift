bfd2e8f1633fe4779e10a6f69b52cddcba9ff132
Crois0509 <crois0509@naver.com>
2024-12-26 17:57:59 +0900

    ✨[feat] 네트워크 매니저 기능 구현

--- PokeDex/PokeDex/Model/NetworkManager.swift
+++ PokeDex/PokeDex/Model/NetworkManager.swift
@@ -6,4 +6,42 @@
 //
 
 import Foundation
+import RxSwift
 
+final class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetch<T: Decodable>(url: URL) -> Single<T> {
+        return Single.create { observer in
+            
+            let session = URLSession(configuration: .default)
+            session.dataTask(with: url) { (data, response, error) in
+                
+                guard let data, error == nil else {
+                    observer(.failure(error!))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse,
+                      (200..<300).contains(response.statusCode)
+                else {
+                    observer(.failure(NetworkError.dataFetchFail))
+                    return
+                }
+                
+                do {
+                    let decodedData = try JSONDecoder().decode(T.self, from: data)
+                    observer(.success(decodedData))
+                    
+                } catch {
+                    print(NetworkError.decodingFail.errorDescription, error)
+                    observer(.failure(NetworkError.decodingFail))
+                }
+                
+            }.resume()
+            
+            return Disposables.create()
+        }
+    }
+}
