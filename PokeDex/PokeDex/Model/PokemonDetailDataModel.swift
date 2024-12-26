795af8074fad7ad3f451073e7e716e82d5eea473
Crois0509 <crois0509@naver.com>
2024-12-26 19:48:24 +0900

    ✨[feat] 포켓몬 디테일 데이터 모델 구현

--- PokeDex/PokeDex/Model/PokemonDetailDataModel.swift
+++ PokeDex/PokeDex/Model/PokemonDetailDataModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct PokemonDetailDataModel: Decodable {
+    let id: Int
+    let name: String
+    let height: Int
+    let weight: Int
+    let sprites: [Sprites]
+    let types: [PokemonTypes]
+}
+
+struct Sprites: Decodable {
+    let frontDefault: String
+    
+    enum CondingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
+
+struct PokemonTypes: Decodable {
+    let slot: Int
+    let type: PokemonType
+}
+
+struct PokemonType: Decodable {
+    let name: String
+}
