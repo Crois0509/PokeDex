f20a8f150f27735ed861252afdf3acd8f306b5d3
Crois0509 <crois0509@naver.com>
2024-12-26 21:06:48 +0900

    ✨[feat] 메인뷰 모델 프로퍼티 세팅

--- PokeDex/PokeDex/ViewModel/MainViewModel.swift
+++ PokeDex/PokeDex/ViewModel/MainViewModel.swift
@@ -5,4 +5,14 @@
 //  Created by 장상경 on 12/26/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+final class MainViewModel {
+    private let limit: Int = 20
+    private let offset: Int = 0
+    
+    let disposeBag = DisposeBag()
+    
+    let pokeDexData = BehaviorSubject(value: [PokemonDataModel]())
+}
