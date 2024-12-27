91b8b4c969db5a8364561d5375a7a57e05fa41c1
Crois0509 <crois0509@naver.com>
2024-12-27 10:01:11 +0900

    ✨[feat] 포켓몬 셀 구현

---
+++ PokeDex/PokeDex/View/PokemonCell.swift
@@ -0,0 +1,48 @@
+//
+//  PokemonCell.swift
+//  PokeDex
+//
+//  Created by 장상경 on 12/27/24.
+//
+
+import UIKit
+import SnapKit
+
+final class PokemonCell: UICollectionViewCell {
+    static let id: String = "PokemonCell"
+    
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        
+        self.addSubview(self.imageView)
+        setupImageView()
+    }
+    
+    private func setupImageView() {
+        self.imageView.backgroundColor = .clear
+        self.imageView.contentMode = .scaleAspectFit
+        
+        self.imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func addImage(_ image: UIImage) {
+        self.imageView.image = image
+    }
+}
