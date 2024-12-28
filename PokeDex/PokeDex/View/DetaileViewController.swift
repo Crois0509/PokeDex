5a1fb10f29e483047f67359b2f99c29da42718fb
Crois0509 <crois0509@naver.com>
2024-12-28 11:14:21 +0900

    📁[file] 포켓몬 디테일 뷰 작업 파일 추가

---
+++ PokeDex/PokeDex/View/DetaileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetaileViewController.swift
+//  PokeDex
+//
+//  Created by 장상경 on 12/28/24.
+//
+
+import Foundation
---
+++ PokeDex/PokeDex/ViewModel/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  PokeDex
+//
+//  Created by 장상경 on 12/28/24.
+//
+
+import Foundation
