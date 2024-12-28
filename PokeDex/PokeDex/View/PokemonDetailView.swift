5e36427c6a85f83b062dee5599a72fa51d94cbf1
Crois0509 <crois0509@naver.com>
2024-12-28 13:16:05 +0900

    ✨[feat] 디테일뷰 데이터 바인딩 추가

--- PokeDex/PokeDex/View/PokemonDetailView.swift
+++ PokeDex/PokeDex/View/PokemonDetailView.swift
@@ -7,31 +7,34 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 final class PokemonDetailView: UIView {
+    
+    private let disposeBag = DisposeBag()
+    
+    private let viewModel: DetailViewModel
     
     private let imageView = UIImageView()
     private let nameLabel = UILabel()
     private let detailLabel = UILabel()
     
-    init(image: UIImage, data: PokemonDetailDataModel) {
+    init(image: UIImage, model: DetailViewModel) {
+        self.viewModel = model
         super.init(frame: .zero)
-        self.imageView.image = image
-        self.nameLabel.text = "No.\(data.id) \(data.name)"
-        self.detailLabel.text = "타입: \(data.types[0].type.name)\n키: \(data.height) m\n몸무게: \(data.weight) kg"
         
         configure()
+        bind()
+        self.imageView.image = image
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        
-        configure()
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func configure() {
         self.backgroundColor = .personalDark
-        self.layer.cornerRadius = 10
+        self.layer.cornerRadius = 20
         self.clipsToBounds = true
         [self.imageView,
          self.nameLabel,
@@ -46,10 +49,11 @@ final class PokemonDetailView: UIView {
     private func setupImage() {
         self.imageView.contentMode = .scaleAspectFit
         self.imageView.backgroundColor = .clear
+        self.imageView.image = UIImage(systemName: "person") // 테스트
     }
     
     private func setupNameLabel() {
-        self.nameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        self.nameLabel.font = UIFont.boldSystemFont(ofSize: 30)
         self.nameLabel.textAlignment = .center
         self.nameLabel.textColor = .white
         self.nameLabel.numberOfLines = 1
@@ -57,7 +61,7 @@ final class PokemonDetailView: UIView {
     }
     
     private func setupDetailLabel() {
-        self.detailLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        self.detailLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
         self.detailLabel.textAlignment = .center
         self.detailLabel.textColor = .white
         self.detailLabel.numberOfLines = 3
@@ -68,7 +72,7 @@ final class PokemonDetailView: UIView {
         self.imageView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(20)
             $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(120)
+            $0.width.height.equalTo(250)
         }
         
         self.nameLabel.snp.makeConstraints {
@@ -80,5 +84,46 @@ final class PokemonDetailView: UIView {
             $0.top.equalTo(self.nameLabel.snp.bottom).offset(10)
             $0.centerX.equalToSuperview()
         }
+    }
+    
+    private func labelConfig(id: Int, name: String, type: String, height: Double, weight: Double) {
+        self.nameLabel.text = "No.\(id) \(name)"
+        self.detailLabel.text = "타입: \(type)\n키: \(height) m\n몸무게: \(weight) kg"
+        self.detailLabel.lineSpacing(10)
+    }
+    
+    private func bind() {
+        self.viewModel.pokemonDetailData
+            .subscribe(onNext: { [weak self] data in
+                guard let self , let data = data.first else { return }
+                guard let type = data.types.first?.type.name else { return }
+                
+                DispatchQueue.main.async {
+                    self.labelConfig(id: data.id,
+                                     name: data.name,
+                                     type: type,
+                                     height: data.height,
+                                     weight: data.weight)
+                }
+                
+            }, onError: { error in
+            
+                
+                
+            }).disposed(by: self.disposeBag)
+    }
+}
+
+extension UILabel {
+    func lineSpacing(_ spacing: CGFloat) {
+        guard let text = self.text else { return }
+        
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        style.alignment = .center
+        attributeString.addAttribute(.paragraphStyle, value: style, range: NSRange(location: 0, length: attributeString.length))
+        
+        self.attributedText = attributeString
     }
 }
--- PokeDex/PokeDex/ViewModel/DetailViewModel.swift
+++ PokeDex/PokeDex/ViewModel/DetailViewModel.swift
@@ -5,4 +5,38 @@
 //  Created by 장상경 on 12/28/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+final class DetailViewModel {
+    private let pokemonManager: PokemonServiceProtocol
+    
+    private let disposeBag = DisposeBag()
+    
+    let pokemonDetailData = BehaviorSubject(value: [PokemonDetailDataModel]())
+    
+    init(pokemonManager: PokemonServiceProtocol, id: Int) {
+        self.pokemonManager = pokemonManager
+        
+        fetchPokemonData(id: id)
+    }
+    
+    private func fetchPokemonData(id: Int) {
+        self.pokemonManager.fetchPokemonList(limit: id, offset: id)
+            .flatMap { [weak self] in
+                guard let self else {
+                    return Single.error(NetworkError.dataFetchFail)
+                }
+                return self.pokemonManager.fetchPokemonDetails($0.results)
+            }
+            .subscribe(onSuccess: { [weak self] (details: [PokemonDetailDataModel]) in
+                guard let self else { return }
+                
+                self.pokemonDetailData.onNext(details)
+                
+            }, onFailure: { error in
+                self.pokemonDetailData.onError(error)
+                
+            }).disposed(by: self.disposeBag)
+    }
+}
