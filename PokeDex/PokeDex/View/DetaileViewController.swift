1ddd7af10130f6cb40eccdd85774e642993c4722
Crois0509 <crois0509@naver.com>
2024-12-28 12:06:09 +0900

    ✨[feat] 디테일 뷰 컨트롤러 구현

--- PokeDex/PokeDex/View/DetaileViewController.swift
+++ PokeDex/PokeDex/View/DetaileViewController.swift
@@ -5,4 +5,42 @@
 //  Created by 장상경 on 12/28/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class DetaileViewController: UIViewController {
+    
+    private let detailView: PokemonDetailView
+    
+    init(detailView: PokemonDetailView) {
+        self.detailView = detailView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    private func configure() {
+        self.navigationController?.navigationBar.isHidden = false
+        self.navigationController?.navigationBar.tintColor = .pointBlue
+        self.view.backgroundColor = .personal
+        self.view.addSubview(self.detailView)
+        
+        setupDetailViewLayout()
+    }
+    
+    private func setupDetailViewLayout() {
+        self.detailView.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.height.equalTo(200)
+        }
+    }
+}
