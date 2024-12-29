8cc16c4cb0b42f7e4cdb9dfbf76e54caf88ef804
Crois0509 <crois0509@naver.com>
2024-12-29 15:40:24 +0900

    💄[style] 문서 주석 추가

--- PokeDex/PokeDex/View/MainViewController.swift
+++ PokeDex/PokeDex/View/MainViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 import SnapKit
 
+// 메인화면 뷰를 보여주는 뷰 컨트롤러
 class MainViewController: UIViewController {
     
-    private lazy var pokemonView = PokeDexView(view: PokemonCollectionView())
+    private lazy var pokemonView = PokeDexView(view: PokemonCollectionView()) // 메인화면 뷰
 
+    // MARK: - MainViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +26,9 @@ class MainViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
     
+    // MARK: - MainViewController UI Setting Method
+    
+    /// VC와 뷰를 세팅하는 메소드
     private func setupUI() {
         self.view.backgroundColor = .personal
         self.view.addSubview(self.pokemonView)
