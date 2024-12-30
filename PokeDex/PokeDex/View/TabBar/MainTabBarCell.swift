5d69ba70551a2a27919f8555aeaf287885662cec
Crois0509 <crois0509@naver.com>
2024-12-30 20:28:29 +0900

    ✨[feat] 탭바 셀 추가

--- PokeDex/PokeDex/View/TabBar/MainTabBarCell.swift
+++ PokeDex/PokeDex/View/TabBar/MainTabBarCell.swift
@@ -5,4 +5,53 @@
 //  Created by 장상경 on 12/30/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MainTabBarCell: UICollectionViewCell {
+    
+    static let id: String = "MainTabBarCell"
+    
+    private let tabLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        configure()
+        setupLabel()
+        setupLayout()
+    }
+    
+    private func configure() {
+        self.backgroundColor = .clear
+        self.clipsToBounds = true
+        self.contentView.addSubview(self.tabLabel)
+    }
+    
+    private func setupLabel() {
+        self.tabLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        self.tabLabel.textColor = .personalDark
+        self.tabLabel.numberOfLines = 1
+        self.tabLabel.textAlignment = .center
+    }
+    
+    private func setupLayout() {
+        self.tabLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func configLabel(title: String) {
+        self.tabLabel.text = title
+    }
+}
