2a79ebe7d8cb9ba9b5294b9f1ff8ef9c52558780
Crois0509 <crois0509@naver.com>
2024-12-31 11:07:12 +0900

    ✨[feat] 검색뷰 추가

--- PokeDex/PokeDex/View/SearchView/SearchView.swift
+++ PokeDex/PokeDex/View/SearchView/SearchView.swift
@@ -5,4 +5,62 @@
 //  Created by 장상경 on 12/31/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class SearchView: UIView {
+    
+    private let searchBar = UITextField()
+    private let searchResultsTableView = SearchTableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupUI()
+    }
+}
+
+private extension SearchView {
+    
+    func setupUI() {
+        configure()
+        setupSearchBar()
+        setupLayout()
+    }
+    
+    func configure() {
+        self.backgroundColor = .clear
+        [self.searchBar, self.searchResultsTableView].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    func setupSearchBar() {
+        self.searchBar.placeholder = "포켓몬의 도감 번호나 이름을 입력해 주세요!"
+        self.searchBar.textColor = .black
+        self.searchBar.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        self.searchBar.borderStyle = .none
+        self.searchBar.layer.cornerRadius = 25
+        self.searchBar.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        self.searchBar.clearButtonMode = .whileEditing
+    }
+    
+    func setupLayout() {
+        self.searchBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        self.searchResultsTableView.snp.makeConstraints {
+            $0.top.equalTo(self.searchBar.snp.bottom).offset(20)
+            $0.trailing.leading.bottom.equalToSuperview()
+        }
+    }
+    
+}
