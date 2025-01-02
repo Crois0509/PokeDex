599cbca8d6cdc17dcf49910200fa6603acf53b8b
Crois0509 <crois0509@naver.com>
2025-01-02 16:11:07 +0900

    ♻️[refactor] 탭바 데이터 바인딩 추가
    델리게이트 메소드를 rx로 변경

--- PokeDex/PokeDex/View/TabBar/MainTabBarController.swift
+++ PokeDex/PokeDex/View/TabBar/MainTabBarController.swift
@@ -9,14 +9,18 @@ import UIKit
 import SnapKit
 import RxSwift
 
+// 탭 바 컨트롤러
 final class MainTabBarController: UIViewController {
     
     private let mainTabBar = MainTabBarView()
     private let disposeBag = DisposeBag()
     
-    private let viewControllers: [UIViewController]
-    private var currentVC: UIViewController?
+    private let viewControllers: [UIViewController] // 탭바가 관리할 뷰 컨트롤러 목록
+    private var currentVC: UIViewController? // 현재 뷰 컨트롤러
     
+    // MARK: - MainTabBarController Initializer
+    
+    // 초기화시 탭 바가 관리할 뷰 컨트롤러 목록을 파라미터로 받음
     init(viewControllers: [UIViewController]) {
         self.viewControllers = viewControllers
         
@@ -27,6 +31,7 @@ final class MainTabBarController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - MainTabBarController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,8 +39,10 @@ final class MainTabBarController: UIViewController {
     }
 }
 
+// MARK: - MainTabBarController UI Setting Method
 private extension MainTabBarController {
     
+    /// 모든 UI를 세팅하는 메소드
     func setupUI() {
         configure()
         setupLayout()
@@ -43,11 +50,13 @@ private extension MainTabBarController {
         bind()
     }
     
+    /// self에 대해 설정하는 메소드
     func configure() {
         self.view.backgroundColor = .personal
         self.view.addSubview(self.mainTabBar)
     }
     
+    /// 모든 레이아웃을 설정하는 메소드
     func setupLayout() {
         self.mainTabBar.snp.makeConstraints {
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
@@ -56,6 +65,8 @@ private extension MainTabBarController {
         }
     }
     
+    /// 현재 뷰 컨트롤러를 바꾸는 메소드
+    /// - Parameter index: 페이지 인덱스
     func displayViewController(_ index: Int) {
         if let currentVC = self.currentVC {
             currentVC.view.removeFromSuperview()
@@ -75,6 +86,7 @@ private extension MainTabBarController {
         self.currentVC = selectedVC
     }
     
+    /// 데이터 바인딩 메소드
     func bind() {
         self.mainTabBar.viewModel.pageIndex
             .withUnretained(self)
--- PokeDex/PokeDex/View/TabBar/MainTabBarView.swift
+++ PokeDex/PokeDex/View/TabBar/MainTabBarView.swift
@@ -7,19 +7,24 @@
 
 import UIKit
 import SnapKit
+import RxCocoa
+import RxSwift
 
+// 탭 바 아이템 스페이스
 enum TabBarItem: String, CaseIterable {
     case main = "도감"
     case search = "검색"
 }
 
+// 탭 바 뷰
 final class MainTabBarView: UIView {
         
-    let viewModel = TabBarViewModel()
+    let viewModel = TabBarViewModel() // 로직 및 데이터 바인딩 객체
+    
+    private let disposeBag = DisposeBag()
     
     private lazy var tabBarView: UICollectionView = {
         let cv = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
-        cv.delegate = self
         cv.dataSource = self
         cv.backgroundColor = .clear
         cv.showsHorizontalScrollIndicator = false
@@ -39,8 +44,9 @@ final class MainTabBarView: UIView {
         return layout
     }()
     
-    private let effectView = SelectEffect()
+    private let effectView = SelectEffect() // 애니메이션 인디케이터
     
+    // MARK: - MainTabBarView Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -54,14 +60,18 @@ final class MainTabBarView: UIView {
     }
 }
 
+// MARK: - MainTabBarView UI Setting Method
 private extension MainTabBarView {
     
+    /// 모든 UI를 세팅하는 메소드
     func setupUI() {
         configure()
         setupTabBarViewLayout()
         setupEffectView()
+        bind()
     }
     
+    /// self에 대해 설정하는 메소드
     func configure() {
         self.backgroundColor = .personal
         self.layer.shadowColor = UIColor.black.cgColor
@@ -72,18 +82,39 @@ private extension MainTabBarView {
         [self.tabBarView, self.effectView].forEach { self.addSubview($0) }
     }
     
+    /// 모든 레이아웃을 세팅하는 메소드
     func setupTabBarViewLayout() {
         self.tabBarView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
+    /// 인디케이터를 세팅하는 메소드
     func setupEffectView() {
         self.effectView.configLabel(title: TabBarItem.main.rawValue)
         let constraintX: CGFloat = (UIScreen.main.bounds.width / 2) / 2 - 75
         self.effectView.frame = .init(x: constraintX, y: 20, width: 150, height: 60)
     }
     
+    /// 데이터 바인딩 메소드
+    func bind() {
+        self.tabBarView.rx.itemSelected
+            .subscribe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] indexPath in
+                
+                self?.viewModel.changePageIndex(indexPath.item)
+                self?.moveEffectView(TabBarItem.allCases[indexPath.item])
+                
+            }, onError: { error in
+                
+                print(error)
+                
+            }).disposed(by: self.disposeBag)
+            
+    }
+    
+    /// 인디케이터의 애니메이션을 설정하는 메소드
+    /// - Parameter title: 인디케이터의 레이블 값
     func moveEffectView(_ title: TabBarItem) {
         self.effectView.configLabel(title: title.rawValue)
         
@@ -99,18 +130,15 @@ private extension MainTabBarView {
     
 }
 
-extension MainTabBarView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.viewModel.changePageIndex(indexPath.item)
-        moveEffectView(TabBarItem.allCases[indexPath.item])
-    }
-}
-
+// MARK: - MainTabBarView UICollectionViewDataSource Method
 extension MainTabBarView: UICollectionViewDataSource {
+    
+    // 셀 수량 설정 메소드
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return TabBarItem.allCases.count
     }
     
+    // 셀 설정 메소드
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainTabBarCell.id, for: indexPath) as? MainTabBarCell else {
             return UICollectionViewCell()
