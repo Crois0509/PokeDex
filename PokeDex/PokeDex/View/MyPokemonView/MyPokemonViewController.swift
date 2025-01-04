1a21c42ebd61dcc21118e96d3d7ff90261eac2ef
Crois0509 <crois0509@naver.com>
2025-01-04 12:59:51 +0900

    ✨[feat] 내 포켓몬 뷰 컨트롤러 설정

--- PokeDex/PokeDex/View/MyPokemonView/MyPokemonViewController.swift
+++ PokeDex/PokeDex/View/MyPokemonView/MyPokemonViewController.swift
@@ -5,4 +5,39 @@
 //  Created by 장상경 on 1/4/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MyPokemonViewController: UIViewController {
+    
+    private let myPokemonView = MyPokemonView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+}
+
+private extension MyPokemonViewController {
+    
+    func setupUI() {
+        configure()
+        setupLayout()
+    }
+    
+    func configure() {
+        self.view.backgroundColor = .personal
+        self.view.addSubview(self.myPokemonView)
+    }
+    
+    func setupLayout() {
+        self.myPokemonView.snp.makeConstraints {
+            $0.top.bottom.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(100)
+        }
+    }
+    
+}
