b11eff50089dff0d203153622001e1d7965516a1
Crois0509 <crois0509@naver.com>
2025-01-04 15:53:34 +0900

    ✨[feat] alert 추가

--- PokeDex/PokeDex/View/AlertManager/AlertManager.swift
+++ PokeDex/PokeDex/View/AlertManager/AlertManager.swift
@@ -7,4 +7,22 @@
 
 import UIKit
 
-
+final class AlertManager {
+    static let alert = AlertManager()
+    private init() {}
+    
+    func showActionSheet(on viewController: UIViewController, message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        viewController.present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+    func showAlert(on viewController: UIViewController, title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        viewController.present(alert, animated: true)
+    }
+}
