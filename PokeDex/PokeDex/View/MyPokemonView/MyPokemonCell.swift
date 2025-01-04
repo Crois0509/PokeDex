024a5379ce0977ae08d4f8323ac3742ff19bd8dc
Crois0509 <crois0509@naver.com>
2025-01-04 15:12:53 +0900

    ✨[feat] 내 포켓몬 셀 설정 메소드 추가

--- PokeDex/PokeDex/View/MyPokemonView/MyPokemonCell.swift
+++ PokeDex/PokeDex/View/MyPokemonView/MyPokemonCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Kingfisher
 
 final class MyPokemonCell: UITableViewCell {
     
@@ -31,6 +32,19 @@ final class MyPokemonCell: UITableViewCell {
         
         setupUI()
     }
+    
+    func configCell(id: Int?, name: String) {
+        if let id = id {
+            guard let url = URL(string: APIEndpoint.pokemonImageURL(id: id).urlString) else { return }
+            self.pokemon.kf.setImage(with: url)
+            self.nameLabel.text = name
+            self.numberLabel.text = "No.\(id)"
+        } else {
+            self.blankLabel.isHidden = false
+            guard let url = URL(string: APIEndpoint.monsterBall.urlString) else { return }
+            self.pokemon.kf.setImage(with: url)
+        }
+    }
 }
 
 private extension MyPokemonCell {
@@ -39,7 +53,7 @@ private extension MyPokemonCell {
         configure()
         setupImageView()
         setupLabel()
-        setupLabel()
+        setupLayout()
     }
     
     func configure() {
@@ -58,7 +72,7 @@ private extension MyPokemonCell {
     func setupImageView() {
         self.pokemon.contentMode = .scaleToFill
         self.pokemon.backgroundColor = .white
-        self.pokemon.layer.cornerRadius = 30
+        self.pokemon.layer.cornerRadius = 25
     }
     
     func setupLabel() {
@@ -72,28 +86,29 @@ private extension MyPokemonCell {
         }
         self.nameLabel.font = UIFont.boldSystemFont(ofSize: 25)
         self.blankLabel.isHidden = true
+        self.blankLabel.text = "빈 슬롯입니다."
     }
     
     func setupLayout() {
         self.pokemon.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(20)
             $0.centerY.equalToSuperview()
-            $0.width.height.equalTo(60)
+            $0.width.height.equalTo(50)
         }
         
         self.numberLabel.snp.makeConstraints {
-            $0.leading.equalTo(self.pokemon.snp.trailing).offset(10)
-            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalTo(self.pokemon.snp.trailing).offset(15)
+            $0.top.equalToSuperview().inset(15)
         }
         
         self.nameLabel.snp.makeConstraints {
             $0.centerX.equalTo(self.numberLabel)
-            $0.top.equalTo(self.numberLabel.snp.bottom).offset(10)
+            $0.bottom.equalToSuperview().inset(15)
         }
         
         self.blankLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.leading.equalTo(self.pokemon.snp.trailing).offset(10)
+            $0.leading.equalTo(self.pokemon.snp.trailing).offset(15)
         }
     }
     
